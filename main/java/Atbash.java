fba83f1d54182382d43383d2a7ac46ffeb9f816c
Saena <toastyhatesthesea@gmail.com>
2018-10-07 23:38:19 -0700

    Added code to encode function, should work

--- main/java/Atbash.java
+++ main/java/Atbash.java
@@ -4,7 +4,35 @@ public class Atbash
 
     public String encode(String aStr)
     {
-        return null;
+        String cleanString = aStr.replaceAll("[ ,.]", "").toLowerCase();
+        String answer = "";
+        int grouping = 0; //used to group letters by groups of 5
+
+        for (int i = 0; i < cleanString.length(); i++)
+        {
+            if (grouping == 5)
+            {
+                answer = answer.concat(" ");
+                grouping = 0; //resets grouping back to zero
+            }
+            char aChar = cleanString.charAt(i);
+            int movement = 0;
+            char aDefault = 'z';
+
+            if (Character.isLetter(aChar))
+            {
+                grouping++;
+                movement = aChar - 'a';
+                aDefault = (char) (aDefault - movement);
+                answer = answer.concat(Character.toString(aDefault));
+            }
+            else if (Character.isDigit(aChar))
+            {
+                grouping++;
+                answer = answer.concat(Character.toString(aChar));
+            }
+        }
+        return answer;
     }
 
     public String decode(String aStr)
