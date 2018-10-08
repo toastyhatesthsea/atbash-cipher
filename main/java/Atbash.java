150d6c5ae133330c2841b7a75433a3ae902c1360
Saena <toastyhatesthesea@gmail.com>
2018-10-07 21:24:09 -0700

    Created Stubs of Decode and encode functions

---
+++ main/java/Atbash.java
@@ -0,0 +1,16 @@
+public class Atbash
+{
+
+
+    public String encode(String aStr)
+    {
+        return null;
+    }
+
+    public String decode(String aStr)
+    {
+        String answer = "";
+
+        return answer;
+    }
+}
